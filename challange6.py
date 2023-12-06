712a9bb6e5c2eaaa88f336aa2b3dd9ba76a5ad17
danielrogel <sagyrogel1@gmail.com>
2023-12-06 10:32:35 +0200

    fixes c6

--- challange6.py
+++ challange6.py
@@ -16,30 +16,19 @@ def parse_line_seconed(line):
 
 
 def roots(a, b, c):
-    sqr_discriminant = (b**2 - 4*a*c)**0.5
-
-    x1 = (-b + sqr_discriminant)/(2*a)
-    x2 = (-b - sqr_discriminant)/(2*a)
-
-    return x1, x2
+    return (-b + (b**2 - 4*a*c)**0.5)/(2*a), (-b - (b**2 - 4*a*c)**0.5)/(2*a)
 
 
 def calc_options(time, distance):
-    a = -1
-    b = time
-    c = -distance
+    a, b, c = -1, time, -distance
     x1, x2 = roots(a, b, c)
     return len(range(int(x1), min(int(x2), time)))
 
 
 def stage1():
     time_arr, distances_arr = [parse_line_first(line) for line in lines]
-
-    options_arr = []
-    for time, distance in zip(time_arr, distances_arr):
-        options_arr.append(calc_options(time, distance))
-
-    print(math.prod(options_arr))
+    print(math.prod([calc_options(time, distance)
+          for time, distance in zip(time_arr, distances_arr)]))
 
 
 def stage2():
